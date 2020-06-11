13010f0bec76eca1496cb65497a5df78bda52938
lestrix <gattinatorshop@gmail.com>
2020-06-11 05:18:02 +0200

    video added

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,29 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { FaGulp } from "react-icons/fa"
-
+import smoke from "../assets/smoke.mp4"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to Green-king.de</p>
+
+    <section>
+      <video playsinline autoPlay muted>
+        <source src={smoke} type="video/mp4" />
+      </video>
+
+      <h1>
+        <span>G</span>
+        <span>R</span>
+        <span>E</span>
+        <span>E</span>
+        <span>N</span>
+        <span> </span>
+        <span>K</span>
+        <span>I</span>
+        <span>N</span>
+        <span>G</span>
+      </h1>
+    </section>
     <FaGulp />
   </Layout>
 )
